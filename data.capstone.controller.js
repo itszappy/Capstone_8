7fcf94269a07e2379aa332111d4dcf13902f3975
Bailey Zaputil <bailey-zaputil@uiowa.edu>
2018-04-16 21:51:09 -0500

    add else if

--- data.capstone.controller.js
+++ data.capstone.controller.js
@@ -54,13 +54,13 @@
                if (response.status == 200) {
                     if (response.data.status == 'error') {
                         alert('error: ' + response.data.message);
-                    } if (response.data.role == 'a') {
+                    } else if (response.data.role == 'a') {
                         $window.location.href = "Admin_Landing.html";
-                    } if (response.data.role == 's') {
+                    } else if (response.data.role == 's') {
                         $window.location.href = "Student_Landing.html";
-                    } if (response.data.role == 't') {
+                    } else if (response.data.role == 't') {
                         $window.location.href = "Tutor_Landing.html";
-                    } if (response.data.role == 'p') {
+                    } else if (response.data.role == 'p') {
                         $window.location.href = "Teacher_Landing.html";
                     }
                } else {
